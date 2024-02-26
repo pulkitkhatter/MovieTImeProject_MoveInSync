e36688e9c25e319ee528bf063cdf87bf5b6c33ed
Pulkit Khatter <pulkitkhatter13@gmail.com>
2024-02-27 00:27:44 +0530

    "Version2 of Project "

--- app/src/main/java/com/example/myapplication/SplashScreen.kt
+++ app/src/main/java/com/example/myapplication/SplashScreen.kt
@@ -4,15 +4,25 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.ProgressBar
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        val backgroundImg: ImageView = findViewById(R.id.logoImageView)
+        val sideAnimation = AnimationUtils.loadAnimation(this, R.anim.slide)
+        backgroundImg.startAnimation(sideAnimation)
+
+
         Handler().postDelayed({
+
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 3000) // 3000 milliseconds = 3 seconds
+        }, 4000) // 4000 milliseconds = 4 seconds
     }
 }
